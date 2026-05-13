1e4e1f24f7ec4a2bd922f58a84ba8fbc34b312f2
eunkyungcho427-png <eunkyungcho427@gmail.com>
2026-05-13 14:33:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -79,18 +79,18 @@ with col3:
 with col4:
     st.markdown("""
         <div class="tool-card">
-            <h3>🔄 FFR PDF리포트 -> Excel리포트</h3>
-            <p>업로드한 PDF리포트에서 LAD, LCX, RCA FFR 수치를 추출해서 엑셀 파일로 변환합니다.</p>
+            <h3>🔄 준비중</h3>
+            <p>준비중입니다.</p>
         </div>
     """, unsafe_allow_html=True)
     
-    if st.button("실행하기", key="btn_test", use_container_width=True):
-        pass
-        try:
+#    if st.button("실행하기", key="btn_test", use_container_width=True):
+#        pass
+#        try:
            # 경로에 'pages/'를 반드시 포함해야 합니다.
-            st.switch_page("pages/4_FFR_PDFtoXLS.py")
-        except Exception as e:
-            st.error("페이지를 찾을 수 없습니다. 파일이 'pages' 폴더 안에 있는지 확인해주세요.")
+#            st.switch_page("pages/4_FFR_PDFtoXLS.py")
+#        except Exception as e:
+#            st.error("페이지를 찾을 수 없습니다. 파일이 'pages' 폴더 안에 있는지 확인해주세요.")
 
 # 5. 하단 안내
 st.divider()
